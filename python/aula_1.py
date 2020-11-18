34430c36ec98a41d79e5ec1ee380ed097b7fe8bf
Carlos Eduardo <kaduchessi@gmail.com>
2020-11-18 17:48:36 -0300

    Add new methods in python for examples

--- python/aula_1.py
+++ python/aula_1.py
@@ -29,6 +29,7 @@ numero_complexo = 3.14j
 print(type(numero_complexo))
 
 # Declaração de uma variável do tipo String
+# String são uma sequencia de caracteres, que pode entender como array
 nome = "Kadu"
 print(nome)
 
@@ -38,3 +39,86 @@ type(nome)
 # Verificando o tipo da variável
 print(type(nome))
 
+# Explicar da que erro somar string com number
+# print(nome + numero_int)
+print(nome + str(numero_int))
+
+# Explicar que consegue pegar parte da string
+# Que sempre o ultimo numero não aparece
+print(nome[0:3])
+
+print(nome[:3])
+
+# Negativo index
+print(nome[-3:-1])
+
+# String length
+print(len(nome))
+
+# String Methods
+# strip
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+# lower
+a = "Hello, World!"
+print(a.lower())
+
+# upper
+a = "Hello, World!"
+print(a.upper())
+
+# replace
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# split
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+# Check string
+# in
+txt = "The rain in Spain stays mainly in the plain"
+x = "ain" in txt
+print(x)
+
+# Check string
+# not in
+txt = "The rain in Spain stays mainly in the plain"
+x = "ain" not in txt
+print(x)
+
+# String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+# String Format
+age = 29
+txt = "My name is Kadu, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+# Escape Character
+# Mostrar o erro
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+# https://docs.python.org/2.5/lib/string-methods.html
