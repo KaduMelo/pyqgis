5cf15963b95868a5865d4b7f4fe00fc5ac1cd927
Carlos Eduardo <kaduchessi@gmail.com>
2020-11-18 16:55:32 -0300

    Add file python to chapter one

---
+++ python/aula_1.py
@@ -0,0 +1,40 @@
+# COMEÇANDO A EXPLICAR INT
+# Explicar o método print
+# Declaração de uma variável do tipo int
+numero_int = 3
+print(type(numero_int))
+
+print(numero_int)
+print(id(numero_int))
+
+# Somando com a variável
+print(numero_int + 3)
+
+# Variável continua com o mesmo valor
+print(numero_int)
+print(id(numero_int))
+
+# Agora se somar colocando na mesma variável numero_int
+numero_int = 3 + 3
+print(numero_int)
+print(id(numero_int))
+
+# COMEÇANDO A EXPLICAR FLOAT
+# Declaração de uma variável do tipo float
+numero_float = 0.3
+print(numero_float + numero_int)
+
+# Declaração de uma variável do tipo complex
+numero_complexo = 3.14j
+print(type(numero_complexo))
+
+# Declaração de uma variável do tipo String
+nome = "Kadu"
+print(nome)
+
+# Porque não imprimiu o resultado?
+type(nome)
+
+# Verificando o tipo da variável
+print(type(nome))
+
